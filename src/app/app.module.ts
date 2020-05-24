d596880a349718680046cf878dc9b53455ff78f8
Ezubot <ezubotz@gmail.com>
2020-05-24 14:37:01 +0200

    added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
-
+import { AppRoutingModule} from './app.routing.module'
 import { AppComponent } from './customer-page/app.component';
 import { InformationBadgeComponent } from './information-badge/information-badge-component' 
 
